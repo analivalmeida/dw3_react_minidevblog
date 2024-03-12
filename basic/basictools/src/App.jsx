1e9c374048b77807b9de2b9d5f90a16576bd7334
SamaraPalladino <luizamaradelg@gmail.com>
2024-03-12 12:06:40 -0300

    Aula 1203 - Eventos Componentes e Renderização.

--- basic/basictools/src/App.jsx
+++ basic/basictools/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
+import Form from './components/Form/Form'
+
 function App() {
   return (
     <>
     <NavBar></NavBar>
+    <Form></Form>
     <Footer></Footer>
     </>
   )
 }
 
 export default App
+
