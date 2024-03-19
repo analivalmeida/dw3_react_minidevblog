ce8960ab92ec4b1762789a954423fde19eb5dec6
analivalmeida <analivialpalmeida@gmail.com>
2024-03-19 12:18:13 -0300

    Aula1903 - Hooks e Listas Arrays

--- basic/basictools/src/components/Form/Form.jsx
+++ basic/basictools/src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+var teste = 8
  const Form = () => {
   const handleMyEvent = (e) =>{
     console.log(e)
@@ -13,26 +13,34 @@ import React from 'react'
     }
   }
   return (
-    <div>
+
+    <>
+        <div>
       <div>
         <button onClick={handleMyEvent}>Clique aqui</button>
       </div>
       <div>
         <button onClick={()=>{console.log('Yes!Apertou em mim.')}}>Clique em mim também!</button>
+      </div>
         <div>
            <button onClick={() =>{
             if (true){
               console.log("Isso não deveria acontecer!")
             }
-          }}>Será que você clicaria aqui?
-           </button>
+          }}>Será que você clicaria aqui?</button>
         </div>
         <div>
           {renderButton(true)}
           {renderButton(false)}
         </div>
-      </div>
-    </div>
+        </div>
+
+        <div>
+          <strong>Valor:</strong>{teste}
+          <button onClick={() =>{ teste = 12; console.log(teste)}}>Aperte e mude o valor</button>
+          <strong>Valor:</strong>{teste}
+        </div>
+    </>
   )
 }
 
