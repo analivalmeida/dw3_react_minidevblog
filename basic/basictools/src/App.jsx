88f190636c6e329f606944f02dc76ba244ff192f
analivalmeida <analivialpalmeida@gmail.com>
2024-04-02 12:35:19 -0300

    Aula 0204 - Styles e Props

--- basic/basictools/src/App.jsx
+++ basic/basictools/src/App.jsx
@@ -4,15 +4,24 @@ import Footer from './components/Footer/Footer'
 import Form from './components/Form/Form'
 import FormHooks from './components/Hooks/FormHooks'
 import ConditionalRender from './components/ConditionalRender/ConditionalRender'
+import ShowUserName from './components/ShowUserName/ShowUserName'
+import { useState } from 'react'
 
 function App() {
+  const [users, setUser] = useState([
+    {active: true, userName: 'Ana'},
+    {active: true, userName: 'Lívia'},
+    {active: false, userName: 'Palma'},
+    {active: true, userName: 'Almeida'}
+  ])
   return (
     <>
-    <NavBar></NavBar>
-    <Form></Form>
-    <Footer></Footer>
-    <FormHooks></FormHooks>
-    <ConditionalRender></ConditionalRender>
+      <NavBar></NavBar>
+      <Form></Form>
+      <Footer></Footer>
+      <FormHooks></FormHooks>
+      <ConditionalRender></ConditionalRender>
+      <ShowUserName list={users}></ShowUserName>
     </>
   )
 }
