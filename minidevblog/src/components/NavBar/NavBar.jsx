2099e3bd1a84210429f6ae2d5e96730f05ff1350
GrazySamy <samanta.graziela@gmail.com>
2024-05-21 12:46:40 -0300

    Aula 2105 Autenticação parte 2 final

--- minidevblog/src/components/NavBar/NavBar.jsx
+++ minidevblog/src/components/NavBar/NavBar.jsx
@@ -1,29 +1,56 @@
-import styles from './NavBar.module.css'
+
 import React from 'react'
+import styles from './NavBar.module.css'
 import { useAuthValue } from '../../context/AuthContext'
 import { userAuthentication } from '../../hooks/userAuthentication'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 export const NavBar = () => {
     const { user } = useAuthValue()
     const { logout } = userAuthentication()
+    const navigate = useNavigate()
 
     return (
         <>
             <nav className={styles.navbar}>
-                <div className={styles.logo}>
-                    <a href='#'>Blog Dev</a>
-                </div>
-                <div className={styles.links}>
-                    <a href='/'>Home</a>
-                    {
-                        !user && (<a href='/login'>Login</a>)
-                    }
-                    {
-                        user && (<a href='/' onClick={logout}>Logout</a>)
-                    }
-                </div>
+                <NavLink to='/' className={styles.brand}>
+                    MiniBlog <span>Dev</span>
+                </NavLink>
+                <ul className={styles.links_list}>
+                    <li>
+                        <NavLink to='/'
+                            className={({ isActive }) => (isActive ? styles.active : null)}> Home
+                        </NavLink>
+                    </li>
+                    {!user && (
+                        <>
+                            <li>
+                                <NavLink to='/login'
+                                    className={({ isActive }) => (isActive ? styles.active : null)}>Login
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
+                    {!user && (
+                        <>
+                            <li>
+                                <NavLink to='/home'
+                                    className={({ isActive }) => (isActive ? styles.active : null)}>Ambiente
+                                </NavLink>
+                            </li>
+                        </>
+                    )}
+                    {!user && (
+                        <>
+                            <li>
+                                <button className={styles.logout} onClick={logout}>Sair</button>
+                            </li>
+                        </>
+                    )}
+                </ul>
             </nav>
         </>
+
     )
 }
 
