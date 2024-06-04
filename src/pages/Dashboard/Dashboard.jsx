ca946ebdf88dee6bc935655962d939ee6723f628
SamaraPalladino <luizamaradelg@gmail.com>
2024-06-04 12:09:36 -0300

    aula0406 Hooks de Firebase terminado

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./Dashboard.module.css"
+import { Link } from 'react-router-dom'
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Dashboard
